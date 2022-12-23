a277d22bef5c83d9fbafb438b5f741c04e8016ad
Chernenko <galka8@inbox.ru>
2022-12-23 14:42:50 +0200

    Change bonus

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,21 @@
 public class Main {
     public static void main(String[] args) {
 
-        int amount = 1004; //сумма пополнения
+        int balans = 100; //сумма баланса
+        int amount = 100; //сумма пополнения
         int percent; // процент бонуса
         if (amount >= 1000)
         {
             percent = 1;
-        } else;
+        } else
         {
             percent = 0;
         }
         int bonus = (amount * percent) / 100;
-        System.out.println(bonus);
+        balans = balans + amount + bonus;
+
+        System.out.println(balans); //сумма баланса
+        System.out.println(bonus); // сумма бонусов
+
     }
 }
